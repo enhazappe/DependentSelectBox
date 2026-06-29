4aa3e80ca8f5fff01bace6d0f4afff4ad7229277
Köhler Milan <kohler@plzen.eu>
2026-06-29 09:05:44 +0200

    Nette/ComponentModel 3.2.0+ compatibility

--- src/DependentTrait.php
+++ src/DependentTrait.php
@@ -65,7 +65,7 @@ trait DependentTrait
 
 		$attrs['data-dependentselectbox-parents'] = Nette\Utils\Json::encode($parents);
 		$attrs['data-dependentselectbox-params'] = Nette\Utils\Json::encode($params);
-		$attrs['data-dependentselectbox'] = $form->getPresenter()->link($this->lookupPath('Nette\\Application\\UI\\Presenter') . Nette\ComponentModel\IComponent::NAME_SEPARATOR . self::SIGNAL_NAME . '!');
+		$attrs['data-dependentselectbox'] = $form->getPresenter()->link($this->lookupPath('Nette\\Application\\UI\\Presenter') . Nette\ComponentModel\IComponent::NameSeparator . self::SIGNAL_NAME . '!');
 
 		$control->addAttributes($attrs);
 		return $control;
